0207199c942d856c548e9220d484e235fb3857e8
NurulHudaZakly <nurulhuza05@gmail.com>
2024-05-20 08:09:37 +0700

    UTS_Microservice

---
+++ order/src/test/java/com/zakly/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakly.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product/src/test/java/com/zakly/product/ProductApplicationTests.java
@@ -0,0 +1,15 @@
+package com.zakly.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
